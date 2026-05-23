839da5bfdbb44d16e3e556d069c04a552b3c6283
Bruno Sergio <brunosergioesouza@gmail.com>
2026-05-23 12:16:21 -0300

    Alterando limite para 100

--- numero-secreto/app.js
+++ numero-secreto/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -14,7 +14,7 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
-exibirMensagemInicial();
+exibirMensagemInicial();    
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
